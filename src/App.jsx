cc973a49003be7433ca3881cb7ef1eb60fbebdd8
VISH01AL <g.vish1al@gmail.com>
2024-09-01 23:19:25 -0400

    started working with Docker

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Styles from './App.module.css'
+
+function App() {
+
+ 
+  return (
+    <div>
+      CAT
+      <img src="https://plus.unsplash.com/premium_photo-1673967831980-1d377baaded2?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="cat image" className={Styles.image}></img>
+    </div>
+  )
+}
+
+export default App
