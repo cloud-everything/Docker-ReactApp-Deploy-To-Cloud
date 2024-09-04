02dcf46ffa55005cbb8d2486d8bc1cf7656f6a62
VISH01AL <g.vish1al@gmail.com>
2024-09-03 21:20:33 -0400

    finished container deployment

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
  
   return (
     <div>
-      CAT
+      CAT 
       <img src="https://plus.unsplash.com/premium_photo-1673967831980-1d377baaded2?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="cat image" className={Styles.image}></img>
     </div>
   )
